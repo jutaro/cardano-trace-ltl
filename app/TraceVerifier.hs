4d43a6b366a3f61466c2e914b9c4f7973344f69c
Russoul <me@russoul.me>
2026-02-12 13:04:28 +0400

    CLI interface

--- app/TraceVerifier.hs
+++ app/TraceVerifier.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE CPP                 #-}
 {-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE ViewPatterns        #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
+{-# LANGUAGE LambdaCase          #-}
 
 module Main(main) where
 
@@ -23,27 +23,23 @@ import           Cardano.Trace.Feed                 (Filename,
 import           Cardano.Trace.Ingest
 import           Control.Concurrent.Async           (async, wait)
 import           Data.Aeson
+import           Data.Aeson.Key                     (toText)
+import qualified Data.Aeson.KeyMap                  as KeyMap
 import           Data.Foldable                      (for_, traverse_)
 import           Data.IORef                         (newIORef)
 import           Data.List                          (find)
-import           Data.Map                           (singleton)
+import qualified Data.Map                           as Map
+import           Data.Map.Strict                    (Map)
 import           Data.Maybe                         (isJust, mapMaybe)
-import           Data.Set                           (fromList)
 import qualified Data.Set                           as Set
 import           Data.Text                          (Text, intercalate, pack,
                                                      unpack)
+import qualified Data.Text                          as Text
 import           Data.Traversable                   (for)
 import           GHC.Generics                       (Generic)
+import           Options.Applicative                hiding (Success)
 import           Streaming
-import           System.Environment                 (getArgs)
 import           System.Exit                        (die)
-import           Text.Read                          (readMaybe)
-import qualified Data.Text as Text
-import Data.Map.Strict (Map)
-import qualified Data.Map as Map
-import qualified Data.Aeson.KeyMap as KeyMap
-import Debug.Trace (trace)
-import Data.Aeson.Key (toText)
 #ifdef METRICS
 import           Control.Concurrent                 (forkIO, killThread,
                                                      threadDelay)
@@ -65,14 +61,14 @@ extractProps = Map.fromList . mapMaybe parse . KeyMap.toList
     parse :: (Key, Value) -> Maybe (PropVarIdentifier, PropValue)
     parse (k, Number v) = Just (toText k, IntValue (truncate v))
     parse (k, String v) = Just (toText k, TextValue v)
-    parse _ = Nothing
+    parse _             = Nothing
 
 instance Event TemporalEvent Text where
   ofTy (TemporalEvent _ msgs _) c = isJust $ find (\msg -> msg.tmsgNS == c) msgs
   index (TemporalEvent _ _ idx) = idx
   props (TemporalEvent _ msgs _) c =
     case find (\msg -> msg.tmsgNS == c) msgs of
-      Just x -> extractProps x.tmsgData
+      Just x  -> extractProps x.tmsgData
       Nothing -> error ("Not an event of type " <> unpack c)
   beg (TemporalEvent t _ _) = t
 
@@ -135,8 +131,8 @@ checkS' phi events = do
       runDisplay next counter
 #endif
 
-checkRealtime :: TemporalEventDurationMicrosec -> Int -> FailureMode -> IngestMode -> [Filename] -> [Formula Text] -> IO ()
-checkRealtime eventDuration minRentionMs failureMode ingestMode files phis = do
+checkOnline :: TemporalEventDurationMicrosec -> Int -> FailureMode -> IngestMode -> [Filename] -> [Formula Text] -> IO ()
+checkOnline eventDuration minRentionMs failureMode ingestMode files phis = do
   ing <- mkIngestor minRentionMs
   for_ files (ingestFileThreaded ing failureMode ingestMode)
   threads <- for phis $ \phi -> async $ do
@@ -144,67 +140,67 @@ checkRealtime eventDuration minRentionMs failureMode ingestMode files phis = do
     checkS' phi (readS reader eventDuration)
   traverse_ wait threads
 
-
--- ☐ ᪲ (∀i. StartLeadershipCheck("slot" = i) ⇒ ♢(1s) (NodeIsLeader("slot" = i) ∨ NodeNotLeader("slot" = i)))
-prop1 :: TemporalEventDurationMicrosec -> Formula Text
-prop1 dur = Forall 0 $ PropForall "i" $
-  Implies
-    (PropAtom "Forge.Loop.StartLeadershipCheck" (fromList [PropConstraint "slot" (Var "i")]))
-    (ExistsN (floor ((1000000 :: Double) / fromIntegral dur)) $
-      Or
-        (PropAtom "Forge.Loop.NodeIsLeader" (fromList [PropConstraint "slot" (Var "i")]))
-        (PropAtom "Forge.Loop.NodeNotLeader" (fromList [PropConstraint "slot" (Var "i")]))
-
-    )
-
--- ☐ ᪲(1s) (∀i. (¬ (NodeIsLeader("slot" = i) ∨ NodeNotLeader("slot" = i)) |˜(1s) StartLeadershipCheck("slot" = i)))
-prop2 :: TemporalEventDurationMicrosec -> Formula Text
-prop2 dur = Forall (floor ((1000000 :: Double) / fromIntegral dur)) $ PropForall "i" $ UntilN
-  (floor ((1000000 :: Double) / fromIntegral dur))
-  (Not $
-    Or
-      (PropAtom "Forge.Loop.NodeIsLeader" (fromList [PropConstraint "slot" (Var "i")]))
-      (PropAtom "Forge.Loop.NodeNotLeader" (fromList [PropConstraint "slot" (Var "i")]))
-  )
-  (PropAtom "Forge.Loop.StartLeadershipCheck" (fromList [PropConstraint "slot" (Var "i")]))
-
--- ☐ ᪲ (∀i. ForgedBlock("slot" = i) ⇒ ♢(3s) AdoptedBlock("slot" = i))
-prop3 :: TemporalEventDurationMicrosec -> Formula Text
-prop3 dur = Forall 0 $ PropForall "i" $ Implies
-  (PropAtom "Forge.Loop.ForgedBlock" (fromList [PropConstraint "slot" (Var "i")]))
-  (ExistsN (floor ((3000000 :: Double) / fromIntegral dur))
-           (PropAtom "Forge.Loop.AdoptedBlock" (fromList [PropConstraint "slot" (Var "i")]))
-  )
-
-data Mode = Online | Offline
+checkOffline :: TemporalEventDurationMicrosec -> Filename -> [Formula Text] -> IO ()
+checkOffline eventDuration file phis = do
+  events <- read file eventDuration
+  threads <- for phis $ \phi -> async $
+    check phi events
+  traverse_ wait threads
 
 
-readMode :: String -> Maybe Mode
-readMode "online"  = Just Online
-readMode "offline" = Just Offline
-readMode _         = Nothing
+data Mode = Online | Offline deriving (Show, Eq)
 
-readArgs :: [String] -> IO (Filename, Mode, TemporalEventDurationMicrosec, [Filename])
-readArgs (phis : (readMode -> Just mode) : (readMaybe -> Just dur) : "--" : traces)
-                                    = pure (phis, mode, dur, traces)
-readArgs _                          = die "Usage: $ <filename> <offline|online> <duration> -- [<filename>]"
+readMode :: ReadM Mode
+readMode = eitherReader $ \case
+  "offline" -> Right Offline
+  "online"  -> Right Online
+  _         -> Left "Expected either of: 'offline' or 'online'"
+
+parseMode :: Parser Mode
+parseMode = option readMode (long "mode" <> metavar "<offline|online>" <> help "mode")
+
+parseEventDuration :: Parser Word
+parseEventDuration = option auto (long "duration" <> metavar "INT" <> help "temporal event duration")
+
+parseFormulasFile :: Parser Filename
+parseFormulasFile = argument str (metavar "FILE")
+
+parseTraceFiles :: Parser [Filename]
+parseTraceFiles = some (argument str (metavar "FILES"))
+
+data CliOptions = CliOptions
+  { formulas      :: Filename
+  , mode          :: Mode
+  , eventDuration :: Word
+  , traces        :: [Filename]}
+
+parseCliOptions :: Parser CliOptions
+parseCliOptions = CliOptions <$> parseFormulasFile <*> parseMode <*> parseEventDuration <*> parseTraceFiles
+
+opts :: ParserInfo CliOptions
+opts = info (parseCliOptions <**> helper)
+  ( fullDesc
+  <> progDesc "Check formula satisfiability against a log of trace messages"
+  <> header "hello - a test for optparse-applicative" )
+
+
+-- | Convert time unit used in the yaml (currently second) input to μs.
+unitToMicrosecond :: Word -> Word
+unitToMicrosecond = (1_000_000 *)
 
 main :: IO ()
 main = do
-  (!formulasFile, !mode, !dur, !traces) <- getArgs >>= readArgs
-  case mode of
+  options <- execParser opts
+  formulas <- readYaml options.formulas Parser.text >>= dieOnYamlException
+  let formulas' = fmap (interpTimeunit (\u -> unitToMicrosecond u `div` fromIntegral options.eventDuration)) formulas
+  case options.mode of
     Offline -> do
-      -- TODO: What shall we do with offline mode?
-      let [file] = traces
-      events <- read file dur
-      check (prop1 dur) events
-      check (prop2 dur) events
-      check (prop3 dur) events
+      file <- case options.traces of
+        [x] -> pure x
+        _   -> die "Only exactly one trace file is supported in 'offline' mode"
+      checkOffline options.eventDuration file formulas'
     Online -> do
-      formulas <- readYaml formulasFile Parser.text >>= dieOnYamlException
-      let formulas' = fmap (interpTimeunit (\sec -> sec * 1_000_000 `div` fromIntegral dur)) formulas
-      for_ formulas' print
-      checkRealtime dur 200 RethrowExceptions FromFileStart traces formulas'
+      checkOnline options.eventDuration 200 RethrowExceptions FromFileStart options.traces formulas'
   where
     dieOnYamlException :: Either Exception [Formula Text] -> IO [Formula Text]
     dieOnYamlException (Left exc) = die (Text.unpack exc)
